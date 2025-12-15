fc2cc5ed215fe403ac24e8d851bd6aaa772d16da
Dipikagithub123 <155236376+Dipikagithub123@users.noreply.github.com>
2025-12-15 11:54:38 +0530

    Simplify CORS middleware configuration
    Replaced specific CORS configuration with default settings.

--- src/server.js
+++ src/server.js
@@ -11,14 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: 'https://vercel-frontend-sweetshop-managemen.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  })
-);
+app.use(cors());
+app.option('*',cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
